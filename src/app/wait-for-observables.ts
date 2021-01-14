983de88a5dab497ffed0af10d8558cbea6f8b351
sajithahd <hdsajitha@gmail.com>
2021-01-14 20:11:41 +0530

    1 file modified

--- src/app/wait-for-observables.ts
+++ src/app/wait-for-observables.ts
@@ -1,14 +1,26 @@
 import { Component } from "@angular/core";
+import { Observable, timer } from "rxjs";
 
 @Component({
-  selector: 'wait-for-observables',
-  template:`
+  selector: "wait-for-observables",
+  template: `
     <div>
       Wait for all observables
     </div>
   `
 })
+export class WaitForObservables {
+  observable1: Observable<any>;
+  observable2: Observable<any>;
 
-export class WaitForObservables{
+  constructor() {
+    this.observable1 = timer(1000,20000);
+    
+    //this.merge();
+  }
 
+  merge(){
+    this.observable1.subscribe(v=> console.log(v))
+  }
+  
 }
