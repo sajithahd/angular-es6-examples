5c1afe0a3a878de6367297e49cc41da4a5ab6837
sajithahd <hdsajitha@gmail.com>
2021-01-14 21:24:05 +0530

    1 file modified

--- src/app/wait-for-observables.ts
+++ src/app/wait-for-observables.ts
@@ -1,36 +1,50 @@
 import { Component, OnDestroy } from "@angular/core";
-import { interval, Observable, Subscription, timer } from "rxjs";
+import { concat, interval, Observable, Subscription, timer } from "rxjs";
 import { map, take } from "rxjs/operators";
 
 @Component({
   selector: "wait-for-observables",
   template: `
     <div>
+      Multiple observables
+      <button (click)="(mulitpleObservables)">Multiple observables</button>
+      <br />
+    </div>
+
+    <div>
       Wait for all observables
+      <button (click)="(mulitpleObservables)">Merge All</button>
+      <br />
     </div>
   `
 })
 export class WaitForObservables implements OnDestroy {
-  observable1: Observable<any>;
-  observable2: Observable<any>;
+  observable1: Observable<number>;
+  observable2: Observable<number>;
   subscription: Subscription;
   subscription2: Subscription;
 
   constructor() {
-    this.observable1 = interval(1000)
+    this.observable1 = timer(1000, 1000)
       .pipe(map(v => v))
       .pipe(take(3));
 
-    this.observable2 = interval(1000)
+    this.observable2 = timer(1000, 1000)
       .pipe(map(v => v))
       .pipe(take(3));
 
-    this.merge();
+    // this.mulitpleObservables();
+    // this.merge();
+  }
+  mulitpleObservables() {
+    this.subscription = this.observable1.subscribe(v => console.log(v));
+    this.subscription2 = this.observable2.subscribe(v => console.log(v));
   }
 
   merge() {
-    this.subscription = this.observable1.subscribe(v => console.log(v));
-    this.subscription2 = this.observable1.subscribe(v => console.log(v));
+    this.observable1
+      .pipe(c => concat(this.observable2))
+      .subscribe(res => console.log(res));
   }
 
   ngOnDestroy(): void {
