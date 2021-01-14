33881d13f4461a40a84de384f93299620507f78a
sajithahd <hdsajitha@gmail.com>
2021-01-14 19:50:38 +0530

    1 file modified

--- src/app/wait-for-observables.ts
+++ src/app/wait-for-observables.ts
@@ -3,10 +3,12 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'wait-for-observables',
   template:`
-    <div></div>
+    <div>
+      wait for all observables
+    </div>
   `
 })
 
 export class WaitForObservables{
-  
+
 }
