2f334206d69782f202f327dd60247494563d428d
sajithahd <hdsajitha@gmail.com>
2021-01-20 21:43:44 +0530

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { FilterComponent } from './filter.component';
-import { WaitForObservables } from './wait-for-observables';
+import { AppComponent } from "./app.component";
+import { FilterComponent } from "./filter.component";
+import { WaitForObservables } from "./wait-for-observables";
+import { MapsAndSetsComponent } from "./maps-and-sets";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, FilterComponent, WaitForObservables ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    FilterComponent,
+    WaitForObservables,
+    MapsAndSetsComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/maps-and-sets.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+ selector:'map-and-set' ,
+ template:`
+  Hellow maps  and sets
+ `
+})
+
+export class MapsAndSetsComponent{
+  
